2d32472c71fff7b7566f86ce9f04ae3884d92cae
sudhirdeo1988 <sudhir.deo1988@gmail.com>
2020-06-26 17:08:16 +0530

    Loader component

---
+++ src/Components/Loader/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Loader.scss';
+
+const Loader = () =>{
+    return(
+        <div className="c-loadtrack">
+            <div className="loader-hideheader" />
+            <div className="loaderline" />
+        </div>
+    );
+}
+
+export default Loader;
--- src/Pages/ErrorPage/ErrorPage.js
+++ src/Pages/ErrorPage/ErrorPage.js
@@ -3,11 +3,13 @@ import './ErrorPage.scss';
 import Header from '../../Components/Header/Header';
 import ErrorContainer from '../../Components/ErrorContainer/ErrorContainer';
 import SliderHolder from '../../Components/SliderHolder/SliderHolder';
+import Loader from '../../Components/Loader/Loader';
 import PlaylistData from '../../DB/viu.json';
 
 const ErrorPage = () =>{
     return (
         <>
+            <Loader />
             <Header header={PlaylistData.header}/>
             <ErrorContainer errorData={PlaylistData.spotlight.error}/>
             <SliderHolder playlist={PlaylistData.videoList} />
